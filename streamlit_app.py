68322c3ef65ca7e64b67122c5cf24d9db42d13e3
forevermoon70-commits <forevermoon70@gmail.com>
2025-07-20 19:10:29 -0500

    Update streamlit_app.py
    just for debug

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from snowflake.snowpark.functions import col
 # {st.__version__}
 st.title(f":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
 st.write(
-  """Choose the fruits you want in your custom Smoothie!
+  """Choose the fruits you want in your custom Smoothie 2!
   """
 )
 
